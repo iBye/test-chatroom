31b6c7d29bced5557d6b61c0f953fb860212cf14
yongod <lyjdra@gmail.com>
2015-11-16 20:15:08 +0800

    添加类文件

--- core.php
+++ core.php
@@ -3,3 +3,9 @@
 /**
  * 负责传送消息
  */
+//include helper类连接数据库
+require "helper.php";
+
+
+echo "Hello World";
+//这里用于处理数据，用Ajax
--- helper.php
+++ helper.php
@@ -6,4 +6,17 @@
 class helper
 {
 
+public $pdo=null;
+
+public function __construct(){
+//初始化数据库
+
+}
+
+    public function connect(){
+        //连接数据库 用PDO
+
+        return $this->pdo;
+    }
+
 }
